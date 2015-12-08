a0a50761ac8834a462b63591e0547179c19a7c87
Jack Willis-Craig <jack.willis-craig@hotmail.com>
2015-12-08 22:48:01 +1100

    Added arandr and font awesome.

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 # basic update
-sudo apt-get -y --force-yes update
-sudo apt-get -y --force-yes upgrade
+#sudo apt-get -y --force-yes update
+#sudo apt-get -y --force-yes upgrade
 
 # install apps
 sudo apt-get -y install \
-  git feh
+  git feh arandr
+
+mkdir ~/Downloads/dotfiles-installer
+cd ~/Downloads/dotfiles-installer
+
+# FONTS
+# Create fonts folder
+mkdir ~/.fonts
+
+## Font Awesome
+wget -O fontawesome.zip https://github.com/FortAwesome/Font-Awesome/archive/v4.5.0.zip
+unzip fontawesome.zip
+rm fontawesome.zip
+cp Font-Awesome-4.5.0/fonts/fontawesome-webfont.ttf ~/.fonts/
