030524b93f3645c4ae2feb65e45b99c548581ed7
Jack Willis-Craig <jack.willis-craig@hotmail.com>
2015-12-08 23:53:38 +1100

    Tried to fix copy bug.

--- install.sh
+++ install.sh
@@ -9,31 +9,31 @@ sudo apt-get install -y \
   git feh arandr thunar rofi compton i3blocks
 
 mkdir ~/Downloads/dotfiles-installer
-cd ~/Downloads/dotfiles-installer
 
 # FONTS
 # Create fonts folder
-mkdir ~/.fonts
+#mkdir ~/.fonts
 
 ## Font Awesome
-wget -O ~/.fonts/fontawesome-webfont.ttf https://github.com/FortAwesome/Font-Awesome/blob/master/fonts/fontawesome-webfont.ttf
+#wget -O ~/.fonts/fontawesome-webfont.ttf https://github.com/FortAwesome/Font-Awesome/blob/master/fonts/fontawesome-webfont.ttf
 
 ## Yosemite San Francisco
-wget -O YosemiteSanFrancisco.zip https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip
-unzip YosemiteSanFrancisco.zip
-cd YosemiteSanFranciscoFont-master
-mv *.ttf ~/.fonts/
+#wget -O YosemiteSanFrancisco.zip https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip
+#unzip YosemiteSanFrancisco.zip
+#cd YosemiteSanFranciscoFont-master
+#mv *.ttf ~/.fonts/
 
 # THEMES
 ## GTK Theme & Icon Theme
-sudo add-apt-repository -y ppa:ravefinity-project/ppa
-sudo apt-get update
-sudo apt-get install -y ambiance-flat-colors radiance-flat-colors vivacious-colors
+#sudo add-apt-repository -y ppa:ravefinity-project/ppa
+#sudo apt-get update
+#sudo apt-get install -y ambiance-flat-colors radiance-flat-colors vivacious-colors
 
 # FILES
-cp -R .config/* ~/.config/
-cp -R .i3/* ~/.i3/
-cp .gtkrc-2.0 ~/.gtkrc-2.0
+DIR=$(dirname $(readlink -f $0))
+cp -R "$DIR"/.config/* ~/.config/
+cp -R "$DIR"/.i3/* ~/.i3/
+cp "$DIR"/.gtkrc-2.0 ~/.gtkrc-2.0
 
 # CLEANUP
 rm -rf ~/Downloads/dotfiles-installer
