97e623d86ff7ded760eb46a3ed13e74139c87705
Jack Willis-Craig <jack.willis-craig@hotmail.com>
2015-12-09 00:09:45 +1100

    Updated directories.

--- install.sh
+++ install.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
 # PRE-INSTALL UPDATE
-#sudo apt-get -y --force-yes update
-#sudo apt-get -y --force-yes upgrade
+sudo apt-get -y --force-yes update
+sudo apt-get -y --force-yes upgrade
 
 # GENERAL
 sudo apt-get install -y \
   git feh arandr thunar rofi compton i3blocks
 
-mkdir ~/Downloads/dotfiles-installer
+DIR=$(dirname $(readlink -f $0))
+TMP="$DIR"/tmp
+mkdir "$TMP"
 
 # FONTS
 # Create fonts folder
-#mkdir ~/.fonts
+mkdir ~/.fonts
 
 ## Font Awesome
-#wget -O ~/.fonts/fontawesome-webfont.ttf https://github.com/FortAwesome/Font-Awesome/blob/master/fonts/fontawesome-webfont.ttf
+FONTAWESOME="$TMP"/FontAwesome
+wget -O "$FONTAWESOME".zip https://github.com/FortAwesome/Font-Awesome/archive/v4.5.0.zip
+unzip "$FONTAWESOME".zip
+mv "$TMP"/Font-Awesome-4.5.0/*.ttf ~/.fonts/
 
 ## Yosemite San Francisco
-#wget -O YosemiteSanFrancisco.zip https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip
-#unzip YosemiteSanFrancisco.zip
-#cd YosemiteSanFranciscoFont-master
-#mv *.ttf ~/.fonts/
+YOSEMITE="$TMP"/YosemiteSanFrancisco
+wget -O "$YOSEMITE".zip https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip
+unzip "$YOSEMITE".zip
+mv "$TMP"/YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/
 
 # THEMES
 ## GTK Theme & Icon Theme
-#sudo add-apt-repository -y ppa:ravefinity-project/ppa
-#sudo apt-get update
-#sudo apt-get install -y ambiance-flat-colors radiance-flat-colors vivacious-colors
+sudo add-apt-repository -y ppa:ravefinity-project/ppa
+sudo apt-get update
+sudo apt-get install -y ambiance-flat-colors radiance-flat-colors vivacious-colors
 
 # FILES
 DIR=$(dirname $(readlink -f $0))
@@ -36,4 +41,4 @@ cp -R "$DIR"/.i3/* ~/.i3/
 cp "$DIR"/.gtkrc-2.0 ~/.gtkrc-2.0
 
 # CLEANUP
-rm -rf ~/Downloads/dotfiles-installer
+rm -rf "$TMP"
