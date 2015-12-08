a279c3f109628b7a6d4e14171b440745a78de374
Jack Willis-Craig <jack.willis-craig@hotmail.com>
2015-12-08 22:51:33 +1100

    Simplified font awesome install.

--- install.sh
+++ install.sh
@@ -16,7 +16,8 @@ cd ~/Downloads/dotfiles-installer
 mkdir ~/.fonts
 
 ## Font Awesome
-wget -O fontawesome.zip https://github.com/FortAwesome/Font-Awesome/archive/v4.5.0.zip
-unzip fontawesome.zip
-rm fontawesome.zip
-cp Font-Awesome-4.5.0/fonts/fontawesome-webfont.ttf ~/.fonts/
+wget -O ~/.fonts/fontawesome-webfont.ttf https://github.com/FortAwesome/Font-Awesome/blob/master/fonts/fontawesome-webfont.ttf
+#wget -O fontawesome.zip https://github.com/FortAwesome/Font-Awesome/archive/v4.5.0.zip
+#unzip fontawesome.zip
+#rm fontawesome.zip
+#cp Font-Awesome-4.5.0/fonts/fontawesome-webfont.ttf ~/.fonts/
